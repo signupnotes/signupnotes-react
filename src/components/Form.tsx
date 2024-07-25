68f8e01a97960da2b3d253596e49e93e52c9c7a4
atilaykosker <atovarkosker@gmail.com>
2024-07-25 11:42:29 +0300

    initial project files and configuration

---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default Form;
